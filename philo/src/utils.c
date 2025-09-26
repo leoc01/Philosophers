8f40dbe2ad301992b0cfc1dcc1c12588d5badd85
leoc01 <leonardocampetti@gmail.com>
2025-09-26 11:40:57 +0100

    chore: change `#include` statements for clarity

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <philo.h>
+#include "philo.h"
 
 void	f_save_times(t_times *times, const char **arg)
 {
